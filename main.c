5dbce7b5f750924e4fca9b1df8a12c71a2caa487
nabil116 <nabilulya1123@gmail.com>
2026-04-13 22:27:41 +0700

    buat file

---
+++ main.c
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+
+int main() {
+    
+    printf("Input Data Mahasiswa\n");
+
+    char nama[100];
+    char npm[100];
+    char jurusan[100];
+    char usia[100];
+    char hobi[100];
+    char alamat[100];
+
+    printf("Nama : ");
+    scanf("%[^\n]", nama);
+
+    printf("NPM : ");
+    scanf(" %[^\n]", npm);
+    
+    printf("Jurusan : ");
+    scanf(" %[^\n]", jurusan);
+    
+    printf("usia : ");
+    scanf(" %[^\n]", usia);
+    
+    printf("hobi : ");
+    scanf(" %[^\n]", hobi);
+    
+    printf("alamat : ");
+    scanf(" %[^\n]", alamat);
